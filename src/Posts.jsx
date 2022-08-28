a6a19b65fd5aba22065316a92ea00a5aa23fecac
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2022-08-27 21:31:32 -0300

    feat: handle error and loading states

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -15,8 +15,9 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery('posts', fetchPosts);
-  if (!data) return null;
+  const { data, error, isError, isLoading } = useQuery('posts', fetchPosts);
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) return <h3>Oops, something went wrong</h3>;
 
   return (
     <>
