7bd3dd0e931cf1bd2366550e72ba24250bdc92d0
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2022-09-04 16:19:32 -0300

    feat: add mutation to updatePost request

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -30,6 +30,8 @@ export function PostDetail({ post }) {
   );
 
   const deleteMutation = useMutation((postId) => deletePost(postId));
+
+  const updateMutation = useMutation((postId) => updatePost(postId));
   
   if (isLoading) return <h3>Loading...</h3>;
   if (isError) return <h3>Oops, something went wrong</h3>;
@@ -40,7 +42,10 @@ export function PostDetail({ post }) {
       {deleteMutation.isError && <p style={{ color: 'red' }}>Error deleting the post</p>}
       {deleteMutation.isLoading && <p style={{ color: 'purple' }}>Deleting the post</p>}
       {deleteMutation.isSuccess && <p style={{ color: 'green' }}>Post has (not) been deleted</p>}
-      <button>Update title</button>
+      <button onClick={() => updateMutation.mutate(post.id)}>Update title</button>
+      {updateMutation.isError && <p style={{ color: 'red' }}>Error updating the post</p>}
+      {updateMutation.isLoading && <p style={{ color: 'purple' }}>Updating the post</p>}
+      {updateMutation.isSuccess && <p style={{ color: 'green' }}>Post has (not) been updated</p>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
